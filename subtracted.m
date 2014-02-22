ae79730408e941cd2fe61351760573c2881c4d9c
victor-dumitrescu <vichvymetal@gmail.com>
2014-02-22 21:00:14 +0000

    Initial masking.

--- subtracted.m
+++ subtracted.m
@@ -2,13 +2,21 @@
 FILE_DIR = 'GOPR0002/';
 filenames = dir([file_dir '*.jpg']);
 
+figure(1); h1 = imshow(frame);
+
 %create bg
-bg = BGSub(25, FILE_DIR);
+% bg = BGSub(25, FILE_DIR);
+bg = RGBNormalize(BGSub(25, FILE_DIR));
 
 for k = 25: size(filenames, 1)
    
     disp([file_dir filenames(k).name]);
-    frame = imread([file_dir filenames(k).name]) - bg;
+%     frame = RGBNormalize(( imread([file_dir filenames(k).name])-bg));
+    frame = RGBNormalize( imread([file_dir filenames(k).name]) );
+    frame = frame - bg;
+    frame(frame<0.075) = 0;
+    frame(frame>=0.075) = 1;
+    
     set(h1, 'CData', frame);
     drawnow('expose');
     
